0b0cf5a2b9bbd588d428274727864378cf66db56
Joren De Groof <git@jorendegroof.be>
2013-02-14 09:27:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testrails::Application.config.secret_key_base = 'b4022ba5e56f9455bf5dc34c40e9b9fc464ef8147b5c5d7c104f6a52b798f9b24d9bb7b918eac2858afed30efa238f13eed90743455ce7ecd49c275ae3e5b20b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testrails::Application.config.session_store :encrypted_cookie_store, key: '_testrails_session'
